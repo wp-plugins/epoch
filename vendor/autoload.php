a70154f7a9a50eeb6c07ee204f73cad2258331fd
Shelob9 <Shelob9@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-07 20:43:39 +0000

    1.0.1 tag
    git-svn-id: https://plugins.svn.wordpress.org/epoch/tags/1.0.1@1215100 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInite66795bcc42a22382bb07181bb2fb177::getLoader();
+return ComposerAutoloaderInitd722254970b8df928ff2bd0499b2b399::getLoader();
